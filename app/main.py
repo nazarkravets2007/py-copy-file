62d396b30c210c0a1ae2823e9342231db4b31445
nazarkravets2007 <nazarfootballer11@gmail.com>
2026-01-21 15:22:59 +0200

    Implement copy_file function
    feat: add file copy functionality

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split()
+
+    if len(parts) != 3:
+        return
+
+    _, src, dst = parts
+
+    if src == dst:
+        return
+
+    with open(src, "r") as file_in, open(dst, "w") as file_out:
+        file_out.write(file_in.read())
